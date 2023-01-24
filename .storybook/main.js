0dfdc51ea842254a28b6519b5b96538220ea6946
Jonathan Caburnay <jonathan.caburnay@outlook.ph>
2023-01-24 18:22:58 +0800

    feature(@src/components): add Task
    Add Task component and corresponding stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.js"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
   ],
   framework: "@storybook/react",
   core: {
@@ -14,4 +15,4 @@ module.exports = {
   features: {
     interactionsDebugger: true,
   },
-};
+}
